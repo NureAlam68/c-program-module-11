136a3f723d00ac1884da8e28c7a8ab577fbd40ff
NureAlam68 <nurealam151068@gmail.com>
2025-05-08 22:50:40 +0600

    string concat

--- string_concat.c
+++ string_concat.c
@@ -5,19 +5,20 @@
 // {
 //     char a[101], b[101];
 //     scanf("%s %s", &a, &b);
-//     int length = strlen(b);
+//     int length_a = strlen(a);
+//     int length_b = strlen(b);
 
-//     for(int i=0; i <= length; i++)
+//     for(int i=0; i <= length_b; i++)
 //     {
-//         a[i] = b[i];
+//         a[i+length_a] = b[i];
 //     }
 
-//     printf("%s %s", a,b);
+//     printf("%s %s", a, b);
 //     return 0;
 // }
 
 
-// use function
+// with function
 #include <stdio.h>
 #include <string.h>
 int main()
@@ -25,7 +26,8 @@ int main()
     char a[101], b[101];
     scanf("%s %s", &a, &b);
 
-    strcpy(a,b);
-    printf("%s %s", a,b);
+    strcat(a, b);
+
+    printf("%s %s", a, b);
     return 0;
 }
